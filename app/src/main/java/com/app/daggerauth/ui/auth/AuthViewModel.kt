59e944e4e76bb05d26983b4c1e724f802674d432
Adarsh Binjola <adarsh.binjola@aspiringminds.in>
2020-08-20 16:05:08 +0530

    sessionmanager  added in viewmodel

--- app/src/main/java/com/app/daggerauth/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/app/daggerauth/ui/auth/AuthViewModel.kt
@@ -5,21 +5,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
+import com.app.daggerauth.SessionManager
 import com.app.daggerauth.models.User
 import com.app.daggerauth.network.auth.AuthApi
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class AuthViewModel @Inject constructor(private val authApi: AuthApi) : ViewModel() {
+class AuthViewModel @Inject constructor(private val authApi: AuthApi,private val sessionManager: SessionManager) : ViewModel() {
 
-private    var _authUser = MediatorLiveData<AuthResource<User>>()
-    val authUser: LiveData<AuthResource<User>> = _authUser
+//private    var _authUser = MediatorLiveData<AuthResource<User>>()
+    val authUser: LiveData<AuthResource<User>> = sessionManager.cachedUser
 
     fun authenticateWithId(userId:Int){
-        _authUser.value= AuthResource.loading()
-        val source : LiveData<AuthResource<User>> = LiveDataReactiveStreams.fromPublisher(authApi.getUser(userId)
+        Log.d("TAG", "authenticateWithId: ")
+                 sessionManager.authenticateWithId(queryUserId(userId))
+    }
+
+   private fun queryUserId(userId: Int): LiveData<AuthResource<User>>{
+      return  LiveDataReactiveStreams.fromPublisher(authApi.getUser(userId)
             .onErrorReturn {
                 var errorUser=User(-1)
                 errorUser
@@ -27,16 +30,10 @@ private    var _authUser = MediatorLiveData<AuthResource<User>>()
                 if(it.id==-1){
                     AuthResource.error("could not authenticate")
                 }else{
-                     AuthResource.authenticated(it)
+                    AuthResource.authenticated(it)
                 }
             }
             .subscribeOn(Schedulers.io()))
-
-        _authUser.addSource(source){
-            _authUser.value=it
-            _authUser.removeSource(source)
-        }
     }
-
 
 }
